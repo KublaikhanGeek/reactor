44142fc1307ac09fc4f064ea18debb621fbfd812
wuzesheng86 <wuzesheng86@a907dffe-942c-7992-d486-e995bdc504d8>
2011-04-23 17:51:57 +0000

    事件驱动框架

---
+++ trunk/event_frame/event.h
@@ -0,0 +1,101 @@
+#ifndef EVENT_FRAME_EVENT_H_
+#define EVENT_FRAME_EVENT_H_
+
+#include <assert.h>
+
+/// @file   event.h
+/// @brief  事件类型定义
+/// @author zeshengwu<wuzesheng@vip.qq.com>
+/// @date   2011-04-25
+
+typedef __int64 uint64_t;
+typedef __int64 int64_t;
+
+namespace event_frame
+{
+/// 任务类
+class Task;
+
+/// 事件基类
+class Event
+{
+public:	
+
+    /// 构造函数
+	Event(Task * task) :
+        m_task(task),
+        m_dispatch_id(0)
+	{}
+
+    /// 析构函数
+	virtual ~Event() {}
+
+    /// 获取事件的分发id
+	uint64_t GetDispatchID() const
+	{
+		return m_dispatch_id;
+	}
+	
+    /// 设置事件分发id
+	void SetDispatchID(uint64_t id)
+	{
+		m_dispatch_id = id;
+	}
+	
+    /// 获取与该事件相关联的task
+	Task * GetTask()
+    {
+        return m_task;
+    }
+	
+    /// 处理事件
+	virtual int Apply() = 0;
+	
+protected:
+	
+    Task *    m_task;         ///< 与此event相关联的task
+    uint64_t  m_dispatch_id;  ///< 通过此id将事件分到不同的workthread
+};
+
+/// 启动指定任务的事件
+template<typename T>
+class StartEvent : public Event
+{
+public:
+
+    /// 构造函数
+	StartEvent(Task * task) :
+		Event(task)
+	{}
+
+    ///  处理启动事件
+	virtual int Apply()
+	{
+        T * task = dynamic_cast<T *>(m_task);
+        assert(task != NULL);
+		return task->ProcessStartEvent(this);
+	}
+};
+
+/// 超时事件
+template<typename T>
+class TimeoutEvent: public Event
+{
+public:
+
+    /// 构造函数
+    TimeoutEvent(Task * task) :
+		Event(task)
+	{}
+
+    /// 处理超时事件
+	virtual int Apply()
+	{
+        T * task = dynamic_cast<T *>(m_task);
+        assert(task != NULL);
+		return task->ProcessTimeoutEvent(this);
+	}
+};
+} // namespace event_frame
+
+#endif // EVENT_FRAME_EVENT_H_
---
+++ trunk/event_frame/event_frame_test.cc
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "event.h"
+#include "task.h"
+
+/// 用户自定义A事件
+template<typename T>
+class UserDefinedAEvent: public event_frame::Event
+{
+public:
+
+    /// 构造函数
+	UserDefinedAEvent(event_frame::Task * task) :
+		event_frame::Event(task)
+	{}
+
+    /// 处理用记自定义A事件
+	virtual int Apply()
+	{
+        T * task = dynamic_cast<T *>(m_task);
+        assert(task != NULL);
+		return task->ProcessUserDefinedAEvent(this);
+	};
+};
+
+/// 用户自定义B事件
+template<typename T>
+class UserDefinedBEvent: public event_frame::Event
+{
+public:
+
+    /// 构造函数
+	UserDefinedBEvent(event_frame::Task * task) :
+		event_frame::Event(task)
+	{}
+
+    /// 处理用记自定义B事件
+	virtual int Apply()
+	{
+        T * task = dynamic_cast<T *>(m_task);
+        assert(task != NULL);
+		return task->ProcessUserDefinedBEvent(this);
+	};
+};
+
+/// 用户自定义的任务
+class UserDefinedTask : public event_frame::Task
+{
+public:
+
+    /// 处理启动事件
+    /// @param  event 启动任务事件
+    /// @retval =0    等待下次事件触发 
+    /// @retval >0    等待超时触发
+    /// @retval <0    任务结束
+	virtual int ProcessStartEvent(event_frame::Event * event)
+	{
+        event_frame::StartEvent<UserDefinedTask> * evt =
+            dynamic_cast<event_frame::StartEvent<UserDefinedTask> *>(event);
+        assert(evt != NULL);
+        /// TODO 处理启动事件
+        return 0;
+    }
+
+    /// 处理超时事件
+    /// @param  event 超时事件
+    /// @retval =0    等待下次事件触发 
+    /// @retval >0    等待超时触发
+    /// @retval <0    任务结束
+    virtual int ProcessTimeoutEvent(event_frame::Event * event)
+	{
+        event_frame::TimeoutEvent<UserDefinedTask> * evt =
+            dynamic_cast<event_frame::TimeoutEvent<UserDefinedTask> *>(event);
+        assert(evt != NULL);
+        /// TODO 处理超时事件
+        return 0;
+    }
+
+    /// 创建一个超时事件
+    /// @return 创建的超时事件的指针
+    virtual event_frame::Event * CreateTimeoutEvent()
+    {
+        return new event_frame::TimeoutEvent<UserDefinedTask>(this);
+    }
+	
+    /// 处理用户自定义A事件
+    /// @param  event 要处理的事件
+    /// @retval =0    等待下次事件触发 
+    /// @retval >0    等待超时触发
+    /// @retval <0    任务结束
+	int ProcessUserDefinedAEvent(UserDefinedAEvent<UserDefinedTask> * event)
+	{
+		/// TODO 处理用户自定义事件
+        fprintf(stderr, "%s\n", "ProcessUserDefinedAEvent");
+        return 0;
+	}
+
+    /// 处理用户自定义B事件
+    /// @param  event 要处理的事件
+    /// @retval =0    等待下次事件触发 
+    /// @retval >0    等待超时触发
+    /// @retval <0    任务结束
+	int ProcessUserDefinedBEvent(UserDefinedBEvent<UserDefinedTask> * event)
+	{
+		/// TODO 处理用户自定义事件
+        fprintf(stderr, "%s\n", "ProcessUserDefinedBEvent");
+        return 0;
+	}
+};
+
+int main()
+{
+    event_frame::Task * task = new UserDefinedTask();
+    event_frame::Event * evt_a = new UserDefinedAEvent<UserDefinedTask>(task);
+    event_frame::Event * evt_b = new UserDefinedBEvent<UserDefinedTask>(task);
+    evt_a->Apply();
+    evt_b->Apply();
+    delete evt_b;
+    delete evt_a;
+    delete task;
+}
