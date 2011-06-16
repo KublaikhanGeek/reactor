d190ce02b5633716a0e982f6e293e762d107ef9a
wuzesheng86 <wuzesheng86@a907dffe-942c-7992-d486-e995bdc504d8>
2011-06-16 16:37:52 +0000

    add partial option

--- pget/pget.py
+++ pget/pget.py
@@ -14,22 +14,45 @@ import optparse
 
 version = '1.0.0'
 
-class PGetOptionParser(optparse.OptionParser):
+class PGetOptionParser(object):
     """class of all the options supported by pget"""
-    def __init__(self):
-        usage = 'Pget %s, a non-interactive network retriever.\n' % version
-        #usage .= 'Usage: %prog [OPTION]... [URL]...\n\n'
-        usage += 'Mandatory arguments to long options are mandatory for short options too.\n\n'
-        optparse.OptionParser(usage)
 
-        group = optparse.OptionGroup(parser, 'Startup')
-        group.add_option('-V', '--version', action='store', type='string',
-                         dest='version', help='display the version of Wget and exit.')
-        group.add_option('-h', '--help', action='callback', callback=self.parser.print_help,
-                         help='print this help.')
-        group.add_option('-e', '--execute', metavar='COMMAND', action='store',
-                         dest='command', help='execute a \`.wgetrc\'-style command.')
+    def __init__(self):
+        usage = 'Pget %s, a non-interactive network retriever.\n' % version + \
+            'Usage: %prog [OPTION]... [URL]...\n'
+        self.parser = optparse.OptionParser(usage)
+        self.init_all_options()
+        self.version = version
+
+    def init_all_options(self):
+        #group startup
+        group = optparse.OptionGroup(self.parser, 'Startup')
+        group.add_option('-V', '--version', action='callback',
+                callback=self.print_version,
+                help='display the version of Wget and exit.')
+        group.add_option('-b', '--background', action='store_true',
+                dest='background', help='go to background after startup.')
+        group.add_option('-e', '--execute', action='store', type='string',
+                dest='command', metavar='CMD',
+                help='execute a \`.wgetrc\'-style command.')
         self.parser.add_option_group(group)
+
+        #group logging and input file
+
+    def parse_args(self, args):
+        (self.values, self.args) = self.parser.parse_args(args)
+
+    def print_version(self, opt, value, parser, *args, **kwargs):
+        print self.version
+
+    def has_option(self, opt_str):
+        return self.parser.has_option(opt_str)
+
+    def get_option_value(self, opt_str):
+        if not self.has_option(opt_str):
+            return None
+        key = self.parser.get_option(opt_str).dest
+        return self.values.__dict__[key]
 
 class Crawler(object):
     """crawler class used to crawl a specified page"""
@@ -42,6 +65,7 @@ class PGet(object):
 def main():
     """the pget entry"""
     parser = PGetOptionParser()
+    parser.parse_args(sys.argv[1:])
 
 if __name__ == '__main__':
     main()
